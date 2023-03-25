ff2af456094e42c89ab0ab66c20d458e7daa4894
pawel_stepniak <pawelstepniak22@gmail.com>
2023-03-25 18:26:22 +0100

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,11 @@ import {  SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser()
 
+  const {data} = api.posts.getAll.useQuery()
+  
   return (
     <>
       <Head>
@@ -23,6 +24,9 @@ const Home: NextPage = () => {
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
     </div>
+      <div>
+      {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+      </div>
 
       </main>
     </>
